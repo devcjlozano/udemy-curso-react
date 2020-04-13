6779d667e680f3265b576dcff8ff9c109e7bd1ed
cjlozano <carlosjlp@gmail.com>
2020-04-13 18:04:42 +0200

    sección 1, 2, 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+// const  Hello = (props) => <p> { props.title }</p> 
+
+/*class Hello extends Component {
+  render () {
+    return <h2> { this.props.title }</h2>
+  }
+}
+
+class Text extends Component {
+  render () {
+    const {
+      arrayOfNumbers,
+      multiply,
+      objectWithInfo,
+      title
+    } = this.props
+  
+    const mappedNumbers = arrayOfNumbers.map(multiply)
+    return (
+      <div>
+        { title }
+        <p> { mappedNumbers.join(', ') }</p>
+        <p> { objectWithInfo.key }</p>
+      </div>
+    )
+  }
+}*/
+
+class Contador extends Component {
+  constructor (props) {
+    super() 
+    this.state = { contador: props.contadorInicial }
+    setInterval(() => {
+      this.setState({ contador: this.state.contador + 1 })
+    }, 1000)
+   }
+   render() {
+      return  <ContadorNumero numero = { this.state.contador} />
+   }
+}
+ 
+Contador.defaultProps = {
+  contadorInicial: 0
+}
+class ContadorNumero extends Component {
+  render () {
+    return <span>{ this.props.numero }</span>
+  }
+}
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <p>Primer componente con state </p>
+        <Contador contadorInicial = { 100 }/>
+      </div>
+    )
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello title='Hello from props'/>
+        <Text
+          arrayOfNumbers= { [2, 3, 10] }
+          objectWithInfo = { { key: 'First Value', key2: 'otherValue'}}
+          isActivated
+          multiply={ number => number * 4}
+          number = {2}
+          text = 'Texto en string'
+          title= { <h1> Este es el titulo </h1> } />
       </header>
     </div>
   );
-}
+} */
 
 export default App;
