d66f806336821804aacfdad513d68517900fc5ad
cjlozano <carlosjlp@gmail.com>
2020-04-22 16:55:34 +0200

    seccion 7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import cars from './data/cars.json';
 import Forms from './sections/forms'
 import './App.css';
@@ -67,7 +68,81 @@ class CardItem extends Component {
   }
 }
 
-//Fomularios
+class Box extends Component {
+  render () {
+    return (
+      <div style= {{ border: '1px solid', margin: 5, padding: 5 }}>
+        { this.props.children }
+      </div>
+    )
+  }
+}
+class Article extends Component {
+  static propTypes = {
+    author: PropTypes.string.isRequired
+  }
+  render () {
+    const {
+      author,
+      children,
+      date,
+      title
+    } = this.props
+    return (
+      <section>
+        <h2>{title}</h2>
+        { author && <p> <em>Escrito por {author}</em></p> }
+        <date> {date}</date>
+        <article>
+          {children}
+        </article>
+      </section>
+    )
+  }
+}
+
+class App extends Component {
+  render () {
+    return (
+      <div className = "App">
+        <h4>Children props</h4>
+        <Article
+          date={new Date().toLocaleDateString()}
+          title='Articulo sobre la prop children'>
+            <p>
+              El contenido que envolvemos dentro del componente Article será enviado
+              al componenente como this.prop.children
+            </p>
+            <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
+        <Article
+          author='Carlos Javier'
+          date={new Date().toLocaleDateString()}
+          title='Articulo 2'>
+            <p>
+              El contenido que envolvemos dentro del componente Article será enviado
+              al componenente como this.prop.children
+            </p>
+            <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
+        <Article
+          author='Carlos Javier'
+          date={new Date().toLocaleDateString()}
+          title='Otro artículo'>
+            <p>
+              El contenido que envolvemos dentro del componente Article será enviado
+              al componenente como this.prop.children
+            </p>
+            <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
+      </div>
+    )
+  }
+}
+
+/* 
+
+Fomularios
 
 class App extends Component {
   render () {
@@ -77,7 +152,7 @@ class App extends Component {
       </div>
     )
   }
-}
+} */
 
 
 /*
